18f9ef8b38bb8a9b71057d4b43781ee74b88ddd8
Akame0xg1rl <ikrambouachra2@gmail.com>
2024-05-19 17:37:36 -0700

    ip

--- console.py
+++ console.py
@@ -2,6 +2,11 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 
@@ -24,10 +29,20 @@ class HBNBCommand(cmd.Cmd):
         if not arg:
             print("** class name missing **")
             return
-        if arg not in globals():
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review
+        }
+        args = arg.split()
+        if args[0] not in classes:
             print("** class doesn't exist **")
             return
-        new_instance = globals()[arg]()
+        new_instance = classes[args[0]]()
         new_instance.save()
         print(new_instance.id)
 
@@ -37,17 +52,17 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in globals():
+        if args[0] not in storage.all():
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         key = "{}.{}".format(args[0], args[1])
-        if key not in storage.all():
+        if key not in storage.all()[args[0]]:
             print("** no instance found **")
             return
-        print(storage.all()[key])
+        print(storage.all()[args[0]][key])
 
     def do_destroy(self, arg):
         "Delete an instance based on class name and id"
@@ -55,29 +70,33 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in globals():
+        if args[0] not in storage.all():
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         key = "{}.{}".format(args[0], args[1])
-        if key not in storage.all():
+        if key not in storage.all()[args[0]]:
             print("** no instance found **")
             return
-        del storage.all()[key]
+        storage.all()[args[0]].pop(key)
         storage.save()
 
     def do_all(self, arg):
         "Show all instances, or all instances of a class"
         args = arg.split()
-        if args and args[0] not in globals():
+        obj_list = []
+        if args and args[0] in storage.all():
+            for value in storage.all()[args[0]].values():
+                obj_list.append(str(value))
+        elif not args:
+            for obj_dict in storage.all().values():
+                for value in obj_dict.values():
+                    obj_list.append(str(value))
+        else:
             print("** class doesn't exist **")
             return
-        obj_list = []
-        for key, obj in storage.all().items():
-            if not args or args[0] == obj.__class__.__name__:
-                obj_list.append(str(obj))
         print(obj_list)
 
     def do_update(self, arg):
@@ -86,14 +105,14 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in globals():
+        if args[0] not in storage.all():
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         key = "{}.{}".format(args[0], args[1])
-        if key not in storage.all():
+        if key not in storage.all()[args[0]]:
             print("** no instance found **")
             return
         if len(args) < 3:
@@ -102,9 +121,8 @@ class HBNBCommand(cmd.Cmd):
         if len(args) < 4:
             print("** value missing **")
             return
-        obj = storage.all()[key]
-        setattr(obj, args[2], args[3])
-        obj.save()
+        setattr(storage.all()[args[0]][key], args[2], args[3])
+        storage.save()
 
 
 if __name__ == '__main__':
